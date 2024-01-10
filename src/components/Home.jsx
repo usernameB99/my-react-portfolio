c99b16c2a2ddd4501b32f84268f4ead26318e917
Bernhard <bernhard.mail.it@gmail.com>
2024-01-10 10:36:59 +0100

    edit text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
-          <h2 className="text-4xl sm:text-6xl font-bold text-white">I'm a Junior Software Developer</h2>
+          <h2 className="text-4xl sm:text-6xl font-bold text-white">I'm a passionate Software Developer</h2>
           <p className="text-gray-500 py-4 max-w-md">
           Hello, I'm Berni!
           My training as a Junior Developer has sparked a deep passion for software development in me. <br />
