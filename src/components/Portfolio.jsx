6774eb3a90fa73e6dfe3492fdef56e6f58b77d71
usernameB99 <kum-a-saufn@live.at>
2024-02-26 11:21:36 +0100

    added projects

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -8,7 +8,13 @@ import portfolioV1 from "../assets/portfolio/portfolioV1.JPG";
 import jdbc from "../assets/portfolio/jdbc.JPG";
 import LiquidCalculator from "../assets/portfolio/LiquidCalculator.jpg";
 import invoiceGenerator from "../assets/portfolio/invoiceGenerator.jpg";
-import Popup from './Popup'
+import inputSaver from "../assets/portfolio/inputSaver.jpg";
+import addToCart from "../assets/portfolio/addToCart.jpg";
+import minimalismBlog from "../assets/portfolio/minimalismBlog.jpg";
+import cyberStore from "../assets/portfolio/cyberStore.jpg";
+import spaceTravel from "../assets/portfolio/spaceTravel.jpg";
+import apartmentSale from "../assets/portfolio/apartmentSale.jpg";
+import Popup from './Popup';
 import { useState } from "react";
 
 
@@ -19,63 +25,105 @@ const Portfolio = () => {
 
   const portfolios = [
     {
-      id: 0,
+      id: 8,
+      src: spaceTravel,
+      demoLink: "https://usernameb99.github.io/space-travel-website/",
+      codeLink: "https://github.com/usernameB99/space-travel-website",
+      hoverText: "current project | css utility class, css variables, resposive web design"
+    },
+    {
+      id: 9,
+      src: cyberStore,
+      demoLink: "https://usernameb99.github.io/cybershop/",
+      codeLink: "https://github.com/usernameB99/cybershop",
+      hoverText: "Designed a product page with ai gen. images | html, css, js"
+    },
+    {
+      id: 10,
+      src: minimalismBlog,
+      demoLink: "https://usernameb99.github.io/minimalistic-minimalism-blog/",
+      codeLink: "https://github.com/usernameB99/minimalistic-minimalism-blog",
+      hoverText: "Designed & created my personal blog | topic: minimalism | html, css"
+    },
+    {
+      id: 11,
+      src: addToCart,
+      demoLink: "https://add-to-cart-berni.netlify.app/",
+      codeLink: "https://github.com/usernameB99/add-to-cart---firebase-app",
+      hoverText: "Mobile Web App | Shopping List for me & Girlfriend | realtime Firebase database"
+    },
+    {
+      id: 11,
+      src: inputSaver,
+      demoLink: "",
+      codeLink: "https://github.com/usernameB99/browser-extension-leads-tracker",
+      hoverText: "Browser Extension | Link Saver | save current tab & notes + delete | Local Storage"
+    },
+    {
+      id: 11,
+      src: apartmentSale,
+      demoLink: "https://doyoutenerife.com/apartment-seaview-tosell/",
+      codeLink: "https://wordpress.com/de/",
+      hoverText: "used my eCommerce knowledge to create apartment sale pages | Wordpress"
+    },
+    {
+      id: 12,
       src: LiquidCalculator,
       demoLink: "https://usernameb99.github.io/liquidCalculator/",
       codeLink: "https://github.com/usernameB99/liquidCalculator",
-      hoverText: "E-Liquid-Calculator I've made with JavaScript"
+      hoverText: "My personal E-Liquid-Calculator for mixing my E-Liquids"
     },
     {
-      id: 1,
+      id: 13,
       src: reactPortfolio,
       demoLink: "https://usernameb99.github.io/portfolio/",
       codeLink: "https://github.com/usernameB99/my-react-portfolio",
-      hoverText: "My current portfolio you were looking at right now :)"
+      hoverText: "Portfolio | React, Tailwind | Mobile Responsiove Web Design"
     },
     {
-      id: 2,
+      id: 14,
       src: needs,
       demoLink: "",
       codeLink: "",
       hoverText: "The project I'm currently working on"
     },
     {
-      id: 3,
+      id: 15,
       src: DgraphReactApp,
       demoLink: "https://usernameb99.github.io/dgraphTest",
       codeLink: "https://github.com/usernameB99/dgraphTest",
       hoverText: "Small frontend with graph databese connection i made for my presentation"
     },
     {
-      id: 4,
+      id: 16,
       src: invoiceGenerator,
       demoLink: "https://usernameb99.github.io/Invoice_Generator/",
       codeLink: "https://github.com/usernameB99/Invoice_Generator",
       hoverText: "Invoice generator with JavaScript"
     },
     {
-      id: 5,
+      id: 17,
       src: rpsExercise,
       demoLink: "https://usernameb99.github.io/RockPaperScissors/",
       codeLink: "https://github.com/usernameB99/RockPaperScissors",
       hoverText: "Rock paper scissors with JavaScript"
     },
     {
-      id: 6,
+      id: 18,
       src: jdbc,
       demoLink: "https://github.com/usernameB99/JDBC",
       codeLink: "https://github.com/usernameB99/JDBC",
       hoverText: "Java personal management with dao classes & data base connection "
     },
     {
-      id: 7,
+      id: 19,
       src: pkmn,
       demoLink: "https://github.com/usernameB99/Pokemon",
       codeLink: "https://github.com/usernameB99/Pokemon",
       hoverText: "Pokemon arena battle in Java with csv reader"
     },
     {
-      id: 8,
+      id: 20,
       src: portfolioV1,
       demoLink: "https://usernameb99.github.io/first-portfolio/",
       codeLink: "https://github.com/usernameB99/usernameB99.github.io",
